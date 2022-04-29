4c00ab1fb344c08827656dcf2b402d13d3c691ea
IlhamBayuAji <102898543+IlhamBayuAji@users.noreply.github.com>
2022-04-29 21:58:02 +0700

    Add files via upload

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type= "disc">
+        <li>Nama Depan : ILHAM</li>
+        <li>Nama Belakang : BAYU AJI</li>
+</ul>
+<br>
+
+<h4>Alamat</h4>
+<ul type= "none">
+    <li> KP. PESING</li>
+</ul>
+
+<h4>Tempat Lahir</h4>
+<ul type= "none">
+    <li>Jakarta</li>
+</ul>
+
+<h4>Olahraga Favorit</h4>
+<ul type= "square">
+    <li>TIDUR</li>
+    <li>TIDUR</li>
+</ul>
+</section>
